aac7fb6f01760f0a2a811e709c152bf2aa4c6db2
Vinicius Ferreira Bezerra <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2023-11-13 20:33:05 -0300

    inicio da atividade

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ const buscarGatinhos = (e) => {
 
     xhr.open( 'GET', 'https://api.thecatapi.com/v1/images/search?limit=10')
     xhr.onreadystatechange = () => {
-        console.log("Aqui1");
         if(xhr.readyState === 4){
-            console.log("Aqui2");
             if (xhr.status === 200) {
-                console.log("Aqui3");
+                let id, altura, largura
                 const cats = JSON.parse(xhr.responseText)
                 console.log(cats);
                 cats.forEach(cat => {
-                    console.log("Aqui4");
                     const img = document.createElement('img')
                     img.src = cat.url
+                    img.id = cat.id
                     document.querySelector('#gatinhos').appendChild(img)
+                    document.querySelector('#gatinhos').appendChild(img.id)
                 });
             } else {
                 alert('Erro na requisição')
