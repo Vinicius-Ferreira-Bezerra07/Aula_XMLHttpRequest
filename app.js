1f86858bb67df0b49cc020453fce631917e5914b
Vinicius Ferreira Bezerra <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2023-11-13 21:44:54 -0300

    Adição do Id da imagem do gato

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const buscarGatinhos = (e) => {
     e.preventDefault()
 
+    // document.write("Tome 10 Gatinhos")
     const xhr = new XMLHttpRequest()
 
     xhr.open( 'GET', 'https://api.thecatapi.com/v1/images/search?limit=10')
     xhr.onreadystatechange = () => {
         if(xhr.readyState === 4){
             if (xhr.status === 200) {
-                let id, altura, largura
+                // let id, altura, largura
                 const cats = JSON.parse(xhr.responseText)
                 console.log(cats);
                 cats.forEach(cat => {
+                    const gato = document.createElement('gato')
                     const img = document.createElement('img')
+                    let idImg = document.createElement('idImg')
+                    
                     img.src = cat.url
                     img.id = cat.id
-                    document.querySelector('#gatinhos').appendChild(img)
+                    idImg.innerHTML = cat.id
+                    
+                    gato.appendChild(img)
+                    gato.appendChild(idImg)
+
+                    console.log(gato);
+                    
+                    document.querySelector('#gatinhos').appendChild(gato)
                 });
             } else {
                 alert('Erro na requisição')
